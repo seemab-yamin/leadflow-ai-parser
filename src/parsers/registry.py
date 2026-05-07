0be0fafa6cc1db0441e4b3c5e0579df75e84d0cf
seemab-yamin <cbyamin806@gmail.com>
2026-05-07 09:59:25 +0800

    Docker Consumer Updated

--- src/parsers/registry.py
+++ src/parsers/registry.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from .base import DocumentParser
 from .dc import DCDocumentParser
 
-
 PARSER_REGISTRY: dict[str, DocumentParser] = {
     "dc": DCDocumentParser(),
 }
